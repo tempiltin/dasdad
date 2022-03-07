85fc273a6499d15802f6deffdba3f331f4fdf1aa
Tempiltin99 <temurbekshukurov0707@gmail.com>
2022-03-07 15:53:25 +0500

    upload

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import "./headre.css";
 import CTA from './CTA';
+import Langu from './Lang';
 import ME from '../../assets/img/man3.png';
 import HeaderSocials from './HeaderSocials';
 const Header = () => {
   return (
     <header id='header'>
+      <div className="MyContainer">
+      <Langu />
+      </div>
       <div className="container">
         <div className="header_container">
-          <h5>Hello ,I'm</h5>
+          <h5 className='hh'>Hello ,I'm</h5>
           <h1>Tempiltin</h1>
-          <h5 className="text-light">Fullstack Developer</h5>
+          <h5 className="text-light">Web Developer</h5>
           <CTA />
           <HeaderSocials />
           <div className="me"> 
---
+++ src/components/header/Lang.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const lang = () => {
+  return (
+    <div className="languages">
+      <a href="#!" className='btn '>O'zbekcha </a>
+      <a href="#!" className='btn btn-primary'>English </a>
+      <a href="#!" className='btn'>Русский </a>
+    </div>
+  )
+}
+
+export default lang
